90bf43fd64df6484c6ec57caf2410a3421b81bbd
Patryk Lenart <deleteman136@gmail.com>
2024-04-04 19:48:09 +0200

    changed div using js

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ const firstName = 'Patryk';
 const secondName = 'Lenart';
 const age = '22';
 console.log(`Siema, nazywam się ${firstName} i mam ${age} lata`);
+
+
+const test = document.querySelector('.block__element--js')
+console.log(test)
+test.innerHTML='treśc zmieniona przy użyciu JS'
